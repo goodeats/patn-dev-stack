7983c1ccebfade295b0e39f283d3cb271fe29f52
Pat Needham <patrick.h.needham@gmail.com>
2025-05-30 10:49:36 -0400

    lint check

--- app/hooks/use-fade-in-on-scroll.ts
+++ app/hooks/use-fade-in-on-scroll.ts
@@ -22,6 +22,7 @@ export function useFadeInOnScroll() {
 
 		return () => {
 			if (ref.current) {
+				// eslint-disable-next-line react-hooks/exhaustive-deps
 				observer.unobserve(ref.current)
 			}
 		}
