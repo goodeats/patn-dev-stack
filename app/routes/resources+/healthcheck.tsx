5f9136162c9babd44de7faec27a46de25395f33a
Pat Needham <patrick.h.needham@gmail.com>
2025-06-24 11:28:52 -0400

    faster healthcheck

--- app/routes/resources+/healthcheck.tsx
+++ app/routes/resources+/healthcheck.tsx
@@ -1,22 +1,23 @@
 // learn more: https://fly.io/docs/reference/configuration/#services-http_checks
 import { prisma } from '#app/utils/db.server.ts'
-import { type Route } from './+types/healthcheck.ts'
 
-export async function loader({ request }: Route.LoaderArgs) {
-	const host =
-		request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
+export async function loader() {
+	// const host =
+	// 	request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
 
 	try {
 		// if we can connect to the database and make a simple query
 		// and make a HEAD request to ourselves, then we're good.
 		await Promise.all([
-			prisma.user.count(),
-			fetch(`${new URL(request.url).protocol}${host}`, {
-				method: 'HEAD',
-				headers: { 'X-Healthcheck': 'true' },
-			}).then((r) => {
-				if (!r.ok) return Promise.reject(r)
-			}),
+			// Just check basic database connectivity
+			prisma.$queryRaw`SELECT 1`,
+			// unnecessary since healthcheck is already hitting this endpoint
+			// fetch(`${new URL(request.url).protocol}${host}`, {
+			// 	method: 'HEAD',
+			// 	headers: { 'X-Healthcheck': 'true' },
+			// }).then((r) => {
+			// 	if (!r.ok) return Promise.reject(r)
+			// }),
 		])
 		return new Response('OK')
 	} catch (error: unknown) {
